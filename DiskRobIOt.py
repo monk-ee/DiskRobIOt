9f580060da6e2b9020689c463f97ac5a25dcefc7
Lyndon Swan <magic.monkee.magic@gmail.com>
2014-10-27 14:30:50 +1000

    added multiprocessing

--- DiskRobIOt.py
+++ DiskRobIOt.py
@@ -24,31 +24,45 @@ GNU General Public License for more details.
 
 import time
 import argparse
+import multiprocessing
 
 
 class DiskRobIOt:
     blocksize = 64
     iterations = 100
+    threads = 2
     diff = 0
 
-    def __init__(self, args):
+    def __init__(self, arg):
         try:
-            self.blocksize = int(args.blocksize)
-            self.iterations = int(args.iterations)
+            self.blocksize = int(arg.blocksize)
+            self.iterations = int(arg.iterations)
+            self.threads = int(arg.threads)
         except:
             raise
-        self._run()
+
+        self.output = multiprocessing.Queue()
+        self.processes = [multiprocessing.Process(target=self._run, args=(x, self.output)) for x in range(int(self.threads))]
+        for p in self.processes:
+            p.start()
+        # Exit the completed processes
+        for p in self.processes:
+            p.join()
+
+        # Get process results from the output queue
+        self.results = [self.output.get() for p in self.processes]
         self._results()
 
-    def _run(self):
-        start_time = time.time()
-        self._file_write_seq_access()
-        stop_time = time.time()
-        self.diff = stop_time - start_time
+    def _run(self, thread_id, output):
+        start_time = time.perf_counter()
+        self._file_write_seq_access(thread_id)
+        stop_time = time.perf_counter()
+        diff = stop_time - start_time
+        output.put(diff)
 
-    def _file_write_seq_access(self):
+    def _file_write_seq_access(self, thread_id):
         chunk = b'\xff' * 1024 * self.blocksize
-        with open("file.file", "wb") as f:
+        with open("file" + str(thread_id) + ".file", "wb") as f:
             for i in range(self.iterations):
                 f.write(chunk)
         f.close()
@@ -56,16 +70,17 @@ class DiskRobIOt:
     def _results(self):
         # now work out write data written
         file_size = 1024 * self.blocksize * self.iterations
-        run_speed = 1 / self.diff
-        run = run_speed * file_size
-        mb = run / 1024 / 1024
-        print(str(mb) + ' Mb/sec with ' + str(self.blocksize) + 'K blocksize.')
-
+        for result in self.results:
+            run_speed = 1 / result
+            run = run_speed * file_size
+            mb = run / 1024 / 1024
+            print(str(mb) + ' Mb/sec with ' + str(self.blocksize) + 'K blocksize.')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A utility for testing disk io.')
     parser.add_argument('--blocksize', default='64', help='The blocksize to write. Defaults to 64(k)')
-    parser.add_argument('--iterations', default='100', help='The number of iterations of chunked writes.')
+    parser.add_argument('--iterations', default='100', help='The number of iterations of chunked writes. Defaults to 100')
+    parser.add_argument('--threads', default='1', help='The number of threads. Defaults to single threaded.')
     args = parser.parse_args()
     dr = DiskRobIOt(args)
