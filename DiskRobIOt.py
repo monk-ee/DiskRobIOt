def1c73725f13c0aa92372cf165761d5767e7c62
Lyndon Swan <magic.monkee.magic@gmail.com>
2014-10-30 15:59:15 +1000

    test file contains all cases and examples of code yet to be put in the main class

--- DiskRobIOt.py
+++ DiskRobIOt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -u
 # -*- coding: utf-8 -*-
 __author__ = 'Monkee Magic <magic.monkee.magic@gmail.com>'
 __version__ = '0.0.1'
@@ -26,6 +26,8 @@ Assumptions:
             iops * transfersizeinbytes = bytespersecond
         then
             iops = bytespersecond / transfersizeinbytes
+
+    bytespersecond =
 """
 
 import time
@@ -35,34 +37,37 @@ from random import shuffle
 import os
 from statistics import mean, median
 
+
 class DiskRobIOt:
     blocksize = 64
     iterations = 100
-    threads = 2
+    file_iterations = 100
     diff = 0
     file_size = 0
     cleanup = False
+    chunk = ""
 
     def __init__(self, arg):
         try:
             self.blocksize = int(arg.blocksize)
             self.iterations = int(arg.iterations)
-            self.threads = int(arg.threads)
-            self.cleanup = bool(arg.cleanup)
+            self.file_iterations = int(arg.fileiterations)
         except:
             raise
-        self.file_size = 1024 * self.blocksize * self.iterations
-        print("writes: ")
-        self._multiplex(self._run_writes)
-        print("reads (sequential): ")
-        self._multiplex(self._run_reads_seq)
-        print("reads (random): ")
-        self._multiplex(self._run_reads_rand)
-        self._cleanup()
+        self.file_size = 1024 * self.blocksize * self.file_iterations
+        self.chunk = 1024 * self.blocksize
+        self._ratioplex()
 
-    def _multiplex(self, call):
+    def _ratioplex(self, write=2, random=4, sequential=4):
+        self.prep_read_files(random, sequential)
         self.output = multiprocessing.Queue()
-        self.processes = [multiprocessing.Process(target=call, args=(x, self.output)) for x in range(int(self.threads))]
+        self.processes = [multiprocessing.Process(target=self._run_low_writes, args=(x, self.output)) for x in
+                          range(write)]
+        self.processes += [multiprocessing.Process(target=self._run_reads_rand, args=(x, self.output)) for x in
+                           range(random)]
+        self.processes += [multiprocessing.Process(target=self._run_reads_seq, args=(x, self.output)) for x in
+                           range(sequential)]
+
         for p in self.processes:
             p.start()
         # Exit the completed processes
@@ -72,96 +77,116 @@ class DiskRobIOt:
         self.results = [self.output.get() for p in self.processes]
         self._results()
 
-    def _run_writes(self, thread_id, output):
+    def prep_read_files(self, random, sequential):
+        for count in range(random):
+            self._file_write_seq_access(count, "read")
+        for count in range(sequential):
+            self._file_write_seq_access(count, "read")
+
+    def _run_low_writes(self, thread_id, output):
         start_time = time.perf_counter()
-        self._file_write_seq_access(thread_id)
+        for iter in range(self.iterations):
+            self._raw_file_write_seq_access(thread_id)
         stop_time = time.perf_counter()
         diff = stop_time - start_time
         output.put(diff)
 
     def _run_reads_seq(self, thread_id, output):
         start_time = time.perf_counter()
-        self._file_read_seq_access(thread_id)
+        for iter in range(self.iterations):
+            self._raw_file_read_seq_access(thread_id)
         stop_time = time.perf_counter()
         diff = stop_time - start_time
         output.put(diff)
 
     def _run_reads_rand(self, thread_id, output):
         start_time = time.perf_counter()
-        self._file_read_random_access(thread_id)
+        for iter in range(self.iterations):
+            self._raw_file_read_random_access(thread_id)
         stop_time = time.perf_counter()
         diff = stop_time - start_time
         output.put(diff)
 
-    def _file_write_seq_access(self, thread_id):
+    def _file_write_seq_access(self, thread_id, name="write"):
         chunk = b'\xff' * 1024 * self.blocksize
-        with open("file" + str(thread_id) + ".file", "wb") as f:
-            for i in range(self.iterations):
+        with open(name + "file" + str(thread_id) + ".file", "wb", 0) as f:
+            for i in range(self.file_iterations):
                 f.write(chunk)
+                f.flush()
+                os.fsync(f.fileno())
         f.close()
 
-    def _file_read_seq_access(self, thread_id):
-        f = open("file" + str(thread_id) + ".file", "rb")
-        chunk = self.blocksize * 1024
-        while True:
-            piece = f.read(chunk)
+    """
+    rawio unbuffered start with close
+    """
+    def _raw_file_write_seq_access(self, thread_id, name="write"):
+        chunkblock = b'\xff' * 1024 * self.blocksize
+        with open(name + "file" + str(thread_id) + ".file", "wb", buffering=0) as f:
+            for i in range(self.file_iterations):
+                f.write(chunkblock)
+                f.flush()
+                os.fsync(f.fileno())
+        f.close()
+
+    def _raw_file_read_seq_access(self, thread_id):
+        locations = list(range(0, self.file_iterations))
+        for position in locations:
+            f = open("readfile" + str(thread_id) + ".file", "rb", buffering=0)
+            f.seek((self.chunk * position), 0)
+            piece = f.read(self.chunk)
+            f.close()
             if not piece:
                 break
-        f.close()
 
-    def _file_read_random_access(self, thread_id):
-        f = open("file" + str(thread_id) + ".file", "rb")
-        chunk = self.blocksize * 1024
-        num_blocks = self.file_size // chunk
-        locations = list(range(1, num_blocks))
+    def _raw_file_read_random_access(self, thread_id):
+        locations = list(range(0, self.file_iterations))
         shuffle(locations)
         for position in locations:
-            f.seek((chunk * position), 0)
-            piece = f.read(chunk)
+            f = open("readfile" + str(thread_id) + ".file", "rb", buffering=0)
+            f.seek((self.chunk * position), 0)
+            piece = f.read(self.chunk)
+            f.close()
             if not piece:
                 break
-        f.close()
+
+    """
+    rawio unbuffered stop
+    """
 
     def _results(self):
-        print("Iterations " + str(self.iterations) + " @ " + str(self.blocksize) + 'K blocksize.')
+        print("Iterations " + str(self.iterations) + " @ " + str(self.blocksize) + 'K blocksize with ' + str(
+            self.file_size / 1024 / 1024) + 'Mb File.')
         # now work out write data written
+        self._iops = 0
+        self._mb = 0
         for result in self.results:
             print(str(self._calculate_mb(result)) + ' Mb/sec ' + str(self._calculate_iops(result)) + ' IOPS')
-            
-        """
+
         meanie = mean(self.results)
-        print("Mean: " + str(self._calculate_mb(meanie)) + ' Mb/sec with ' + str(self.blocksize) + 'K blocksize.')
-        print("Mean: " + str(self._calculate_iops(meanie)) + ' IOPS with ' + str(self.blocksize) + 'K blocksize.')
+        print("Mean: " + str(self._calculate_mb(meanie)) + ' Mb/sec ' + str(self._calculate_iops(meanie)) + ' IOPS')
         medie = median(self.results)
-        print("Median: " + str(self._calculate_mb(medie)) + ' Mb/sec with ' + str(self.blocksize) + 'K blocksize.')
-        print("Median: " + str(self._calculate_iops(medie)) + ' IOPS with ' + str(self.blocksize) + 'K blocksize.')
-        """
+        print("Median: " + str(self._calculate_mb(medie)) + ' Mb/sec ' + str(self._calculate_iops(medie)) + ' IOPS')
+
 
     def _calculate_mb(self, result):
-        run_ratio = 1 / result
+        run_ratio = 1 / (result / self.iterations)
         run = run_ratio * self.file_size
         mb = run / 1024 / 1024
         return mb
 
     def _calculate_iops(self, result):
-        print(result)
-        run_ratio = 1 / result
-        run = run_ratio * self.file_size
-        print(run)
-        iops = run / self.file_size
+        run_ratio = 1 / (result / self.iterations)
+        iops = (run_ratio * self.iterations)
+        # iops = run / self.file_size
         return iops
-
-    def _cleanup(self):
-        if self.cleanup:
-            for x in range(int(self.threads)):
-                os.remove("file" + str(x) + ".file")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A utility for testing disk io.')
     parser.add_argument('--blocksize', default='64', help='The blocksize to write. Defaults to 64(k)')
-    parser.add_argument('--iterations', default='100', help='The number of iterations of chunked writes. Defaults to 100')
-    parser.add_argument('--threads', default='1', help='The number of threads. Defaults to single threaded.')
-    parser.add_argument('--cleanup', default='1', help='Deleted files created for testing. Default is False.')
+    parser.add_argument('--fileiterations', default='100',
+                        help='The number of iterations of chunked writes. Defaults to 100')
+    parser.add_argument('--iterations', default='100', help='The number of times to run the test. Defaults to 100')
+
     args = parser.parse_args()
     dr = DiskRobIOt(args)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -u
 # -*- coding: utf-8 -*-
 __author__ = 'Monkee Magic <magic.monkee.magic@gmail.com>'
 __version__ = '0.0.1'
@@ -36,31 +36,44 @@ import multiprocessing
 from random import shuffle
 import os
 from statistics import mean, median
+import io
+
 
 class DiskRobIOt:
     blocksize = 64
     iterations = 100
+    file_iterations = 100
     threads = 2
     diff = 0
     file_size = 0
     cleanup = False
+    chunk = ""
 
     def __init__(self, arg):
         try:
             self.blocksize = int(arg.blocksize)
             self.iterations = int(arg.iterations)
+            self.file_iterations = int(arg.fileiterations)
             self.threads = int(arg.threads)
-            self.cleanup = bool(arg.cleanup)
+            self.cleanup = arg.cleanup
         except:
             raise
-        self.file_size = 1024 * self.blocksize * self.iterations
-        print("writes: ")
-        self._multiplex(self._run_writes)
+        self.file_size = 1024 * self.blocksize * self.file_iterations
+        self.chunk = 1024 * self.blocksize
+        self._ratioplex()
+        # print("high writes: ")
+        #self._multiplex(self._run_high_writes)
+        #print("low writes: ")
+        #self._multiplex(self._run_low_writes)
+        """
         print("reads (sequential): ")
         self._multiplex(self._run_reads_seq)
         print("reads (random): ")
         self._multiplex(self._run_reads_rand)
-        self._cleanup()
+        #print("reads/writes: ")
+        #self._multiplex(self._run_reads_writes)
+        """
+        #self._cleanup()
 
     def _multiplex(self, call):
         self.output = multiprocessing.Queue()
@@ -74,38 +87,74 @@ class DiskRobIOt:
         self.results = [self.output.get() for p in self.processes]
         self._results()
 
-    def _run_writes(self, thread_id, output):
+    def _ratioplex(self, write=2, random=4, sequential=4):
+        self.prep_read_files(random, sequential)
+        self.output = multiprocessing.Queue()
+        self.processes = [multiprocessing.Process(target=self._run_low_writes, args=(x, self.output)) for x in
+                          range(write)]
+        self.processes += [multiprocessing.Process(target=self._run_reads_rand, args=(x, self.output)) for x in
+                           range(random)]
+        self.processes += [multiprocessing.Process(target=self._run_reads_seq, args=(x, self.output)) for x in
+                           range(sequential)]
+
+        for p in self.processes:
+            p.start()
+        # Exit the completed processes
+        for p in self.processes:
+            p.join()
+        # Get process results from the output queue
+        self.results = [self.output.get() for p in self.processes]
+        self._results()
+
+    def prep_read_files(self, random, sequential):
+        for count in range(random):
+            self._file_write_seq_access(count, "read")
+        for count in range(sequential):
+            self._file_write_seq_access(count, "read")
+
+    def _run_high_writes(self, thread_id, output):
         start_time = time.perf_counter()
         self._file_write_seq_access(thread_id)
         stop_time = time.perf_counter()
-
         diff = stop_time - start_time
-        print(str(start_time) + " / "+ str(stop_time) + " / " + str(diff))
+        output.put(diff)
+
+    def _run_low_writes(self, thread_id, output):
+        start_time = time.perf_counter()
+        for iter in range(self.iterations):
+            self._raw_file_write_seq_access(thread_id)
+        stop_time = time.perf_counter()
+        diff = stop_time - start_time
         output.put(diff)
 
     def _run_reads_seq(self, thread_id, output):
         start_time = time.perf_counter()
-        self._file_read_seq_access(thread_id)
+        for iter in range(self.iterations):
+            self._raw_file_read_seq_access(thread_id)
         stop_time = time.perf_counter()
         diff = stop_time - start_time
         output.put(diff)
 
     def _run_reads_rand(self, thread_id, output):
         start_time = time.perf_counter()
-        self._file_read_random_access(thread_id)
+        for iter in range(self.iterations):
+            self._raw_file_read_random_access(thread_id)
         stop_time = time.perf_counter()
         diff = stop_time - start_time
         output.put(diff)
 
-    def _file_write_seq_access(self, thread_id):
+
+    def _file_write_seq_access(self, thread_id, name="write"):
         chunk = b'\xff' * 1024 * self.blocksize
-        with open("file" + str(thread_id) + ".file", "wb") as f:
-            for i in range(self.iterations):
+        with open(name + "file" + str(thread_id) + ".file", "wb", 0) as f:
+            for i in range(self.file_iterations):
                 f.write(chunk)
+                f.flush()
+                os.fsync(f.fileno())
         f.close()
 
     def _file_read_seq_access(self, thread_id):
-        f = open("file" + str(thread_id) + ".file", "rb")
+        f = open("readfile" + str(thread_id) + ".file", "rb", buffering=0)
         chunk = self.blocksize * 1024
         while True:
             piece = f.read(chunk)
@@ -114,7 +163,7 @@ class DiskRobIOt:
         f.close()
 
     def _file_read_random_access(self, thread_id):
-        f = open("file" + str(thread_id) + ".file", "rb")
+        f = open("readfile" + str(thread_id) + ".file", "rb", buffering=0)
         chunk = self.blocksize * 1024
         num_blocks = self.file_size // chunk
         locations = list(range(1, num_blocks))
@@ -126,8 +175,84 @@ class DiskRobIOt:
                 break
         f.close()
 
+    """
+    File os start
+    """
+    def _fd_file_write_seq_access(self, thread_id, name="write"):
+        if name == "read" and os.path.isfile(name + "file" + str(thread_id) + ".file") and os.path.getsize(name + "file"
+                + str(thread_id) + ".file") == self.file_size:
+            return
+        chunkblock = b'\xff' * self.chunk
+        fd = os.open(name + "file" + str(thread_id) + ".file", os.O_WRONLY|os.O_CREAT)
+        for i in range(self.file_iterations):
+            os.write(fd, chunkblock)
+            os.fsync(fd)
+        os.close(fd)
+
+    def _fd_file_read_seq_access(self, thread_id):
+        fd = os.open("readfile" + str(thread_id) + ".file", os.O_RDONLY)
+        while True:
+            piece = os.read(fd, self.chunk)
+            if not piece:
+                break
+        os.close(fd)
+
+    def _fd_file_read_random_access(self, thread_id):
+        fd = os.open("readfile" + str(thread_id) + ".file", os.O_RDONLY)
+        locations = list(range(1, self.file_iterations))
+        shuffle(locations)
+        for position in locations:
+            os.lseek(fd, (self.chunk * position), 0)
+            piece = os.read(fd, self.chunk)
+            if not piece:
+                break
+        os.close(fd)
+
+    """
+    file os stop
+    """
+
+    """
+    rawio unbuffered start
+    """
+    def _raw_file_write_seq_access(self, thread_id, name="write"):
+        chunkblock = b'\xff' * 1024 * self.blocksize
+        with open(name + "file" + str(thread_id) + ".file", "wb", buffering=0) as f:
+            for i in range(self.file_iterations):
+                f.write(chunkblock)
+                f.flush()
+                os.fsync(f.fileno())
+        f.close()
+
+    def _raw_file_read_seq_access(self, thread_id):
+        locations = list(range(0, self.file_iterations))
+        for position in locations:
+            f = open("readfile" + str(thread_id) + ".file", "rb", buffering=0)
+            f.seek((self.chunk * position), 0)
+            piece = f.read(self.chunk)
+            f.close()
+            if not piece:
+                break
+
+    def _raw_file_read_random_access(self, thread_id):
+        locations = list(range(0, self.file_iterations))
+        shuffle(locations)
+        for position in locations:
+            f = open("readfile" + str(thread_id) + ".file", "rb", buffering=0)
+            f.seek((self.chunk * position), 0)
+            piece = f.read(self.chunk)
+            f.close()
+            if not piece:
+                break
+
+
+    """
+    rawio unbuffered stop
+    """
+
     def _results(self):
-        print("Iterations " + str(self.iterations) + " @ " + str(self.blocksize) + 'K blocksize.')
+        print("Iterations " + str(self.iterations) + " @ " + str(self.blocksize) + 'K blocksize with ' + str(
+            self.file_size / 1024 / 1024) + 'Mb File.')
         # now work out write data written
         self._iops = 0
         self._mb = 0
@@ -138,23 +263,18 @@ class DiskRobIOt:
         print("Mean: " + str(self._calculate_mb(meanie)) + ' Mb/sec ' + str(self._calculate_iops(meanie)) + ' IOPS')
         medie = median(self.results)
         print("Median: " + str(self._calculate_mb(medie)) + ' Mb/sec ' + str(self._calculate_iops(medie)) + ' IOPS')
-        sumie = sum(self.results)
-        print("Sum: " + str(self._calculate_mb(sumie)) + ' Mb/sec ' + str(self._calculate_iops(sumie)) + ' IOPS')
-        print("Cumulative: " + str(self._mb) + ' Mb/sec ' + str(self._iops) + ' IOPS')
+
 
     def _calculate_mb(self, result):
-        print(result)
-        run_ratio = self.file_size / result
-        #run = run_ratio * self.file_size
-        mb = run_ratio / 1024 / 1024
-        self._mb = self._mb + mb
+        run_ratio = 1 / (result / self.iterations)
+        run = run_ratio * self.file_size
+        mb = run / 1024 / 1024
         return mb
 
     def _calculate_iops(self, result):
-        run_ratio = self.file_size / result
-        #run = run_ratio * self.file_size
-        iops = run_ratio / self.file_size
-        self._iops = self._iops + iops
+        run_ratio = 1 / (result / self.iterations)
+        iops = (run_ratio * self.iterations)
+        # iops = run / self.file_size
         return iops
 
     def _cleanup(self):
@@ -166,8 +286,10 @@ class DiskRobIOt:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A utility for testing disk io.')
     parser.add_argument('--blocksize', default='64', help='The blocksize to write. Defaults to 64(k)')
-    parser.add_argument('--iterations', default='100', help='The number of iterations of chunked writes. Defaults to 100')
+    parser.add_argument('--fileiterations', default='100',
+                        help='The number of iterations of chunked writes. Defaults to 100')
+    parser.add_argument('--iterations', default='100', help='The number of times to run the test. Defaults to 100')
     parser.add_argument('--threads', default='1', help='The number of threads. Defaults to single threaded.')
-    parser.add_argument('--cleanup', default='False', help='Deleted files created for testing. Default is False.')
+    parser.add_argument('--cleanup', dest='cleanup', action='store_true')
     args = parser.parse_args()
     dr = DiskRobIOt(args)
